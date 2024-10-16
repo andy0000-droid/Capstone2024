b1747963f7389e3fee4be310ae402afc4358dbe9
andy0000-droid <andy020207@gmail.com>
2024-10-16 15:10:39 +0900

    Split Branch

--- aria-private/ARIA_test/dllload.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <windows.h>
-#define DLL_PROC_SETUP(M) BOOL _dllprocloaded = TRUE; HMODULE _dllmodule = M;
-#define DLL_PROC_LOAD(VAR, LOAD_NAME, LOAD_TYPE) (_dllprocloaded = _dllprocloaded && ((VAR = (LOAD_TYPE)GetProcAddress(_dllmodule, LOAD_NAME)) != NULL))
-#define DLL_PROC_RESULT(LOADED) (LOADED = _dllprocloaded)
